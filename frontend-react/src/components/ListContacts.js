8e9d505c759cec6dc4077e49424bf79975bcb20e
sultanmyrza <sultanmyrza@gmail.com>
2018-04-13 14:44:13 +0900

    add search functionality with regex

--- frontend-react/src/components/ListContacts.js
+++ frontend-react/src/components/ListContacts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends React.Component {
     static propTypes = {
@@ -16,6 +18,17 @@ class ListContacts extends React.Component {
     }
      
     render() {
+        const { contacts, onDeleteContact } = this.props;
+        const { query } = this.state
+
+        let showingContacts;
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i');
+            showingContacts = contacts.filter((contact) => match.test(contact.name))
+        } else {
+            showingContacts = contacts;
+        }
+
         return (
             <div className="list-contacts">
                 <div className="list-contacts-top">
@@ -23,12 +36,12 @@ class ListContacts extends React.Component {
                         className="search-contacts"
                         type="text"
                         placeholder="Search contacts"
-                        value={this.state.query}
+                        value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
                 <ol className="contact-list">
-                    {this.props.contacts.map((contact) => (
+                    {showingContacts.map((contact) => (
                         <li key={contact.id} className="contact-list-item">
                             <div className="contact-avatar" style={{ backgroundImage:`url(${contact.avatarURL})`}}/>
                             <div className="contact-details">
@@ -36,7 +49,7 @@ class ListContacts extends React.Component {
                                 <p>{contact.email}</p>
                             </div>
                             <button 
-                                onClick={() => this.props.onDeleteContact(contact)}
+                                onClick={() => onDeleteContact(contact)}
                                 className="contact-remove">
                                 Remove
                             </button>
