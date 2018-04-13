c8e36384710ec93791b09c614ed698d6257d5a93
sultanmyrza <sultanmyrza@gmail.com>
2018-04-13 15:10:23 +0900

    refactor get contacts from server

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
 import './index.css';
 import ListContacts from './components/ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 
 class App extends Component {
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({contacts})
+    })
+  }
+
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ],    
+    contacts: []    
   }
 
   removeContact = (contactToRemove) => {
