41d9e48a46eba29b7e34ab6881717d964a28af42
sultanmyrza <sultanmyrza@gmail.com>
2018-04-14 11:56:00 +0900

    add form to create contact

--- frontend-react/src/components/CreateContact.js
+++ frontend-react/src/components/CreateContact.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends React.Component {
+    handleSubmit = (event) => {
+        // prevent submit to manually serialize submitted data
+        event.preventDefault();
+        console.log('event prevented');
+        const formValues = serializeForm(event.target, {hash: true});
+        console.log(formValues);
+    }
     render() {
         return (
-            <div>Create contact</div>
+            <div>
+                <Link to="/" className="close-create-contact">
+                    Close
+                </Link>
+                <form onSubmit={this.handleSubmit} className="create-contact-form">
+                    <ImageInput
+                        className="create-contact-avatar-input"
+                        name="avatarURL"
+                        maxHeight={64}/>
+                    <div className="create-contact-details">
+                        <input type="text" name="name" placeholder="Name"/>
+                        <input type="text" name="email" placeholder="Email"/>
+                        <button>Add contact</button>
+                    </div>
+                </form> 
+            </div>
         )
     }
 }
